eb7c26ca6c8a33a8741e957ea508616cda07967f
Ziyad Khalil <ziyadkhalil@gmail.com>
2021-08-14 15:57:00 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Entry } from "./src/navigation/entry";
+import { colors, theme } from "@src/theme";
+
+const queryClient = new QueryClient();
+
+const nativeBaseConfig = {
+  dependencies: {
+    "linear-gradient": require("expo-linear-gradient").LinearGradient,
+  },
+  suppressColorAccessibilityWarning: true,
+  theme,
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <NativeBaseProvider config={nativeBaseConfig}>
+        <SafeAreaProvider style={{ backgroundColor: colors.background }}>
+          <NavigationContainer>
+            <Entry />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </NativeBaseProvider>
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
